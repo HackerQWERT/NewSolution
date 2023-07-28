5e46a2fe8a04bb53bbb6959ad72833552062c3a6
Q F <HackerQWERT@outlook.com>
2023-07-28 11:22:49 +0800

    Resolve conflict in Kafka/Program.cs

--- Kafka/Program.cs
+++ Kafka/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+        using (var producer = new ProducerBuilder<Null, string>(config).Build())
+        {
+            string topic = "your_topic_name";
+            string message = "Hello, Kafka!";
+            var deliveryReport = producer.ProduceAsync(topic, new Message<Null, string> { Value = message });
+            deliveryReport.ContinueWith(task =>
+            {
+                Console.WriteLine($"Message delivered: {task.Result.TopicPartitionOffset}");
+            });
+        }
+    }
+}
